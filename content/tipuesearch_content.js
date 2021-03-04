6bf833d2e80a0dfebd5dfbb632d35544f7b3f4ca
40923123 <40923123@gm.nfu.edu.tw>
2021-03-04 11:44:27 +0800

    two week page update

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': '第一周', 'text': '', 'tags': '', 'url': '第一周.html'}, {'title': '第二周', 'text': '', 'tags': '', 'url': '第二周.html'}]};
+var tipuesearch = {"pages": [{'title': '第一周', 'text': '', 'tags': '', 'url': '第一周.html'}, {'title': '第二周', 'text': 'onshape 一個將工具放在網路上使用 \n 新的網站是可以使用https://localhost:9443但如果在其他環境下則需要自己去找IPV6的位置才能完成同步協調', 'tags': '', 'url': '第二周.html'}]};
